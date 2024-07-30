a8fb347095b8baeeb72da21dcc371be042708971
Shaik Sandani Basha <shaiksandanibasha7@gmail.com>
2024-07-30 09:44:56 +0000

    Changed

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-// Get the button and audio elements
-const viewProjectsBtn = document.getElementById('viewProjectsBtn')
-const audio = new Audio(
-  'https://res.cloudinary.com/dq4yjeejc/video/upload/v1722134393/beat_2_e8o29r.wav',
-)
+document.addEventListener('DOMContentLoaded', () => {
+  const viewProjectsBtn = document.getElementById('viewProjectsBtn')
+  const audio = new Audio(
+    'https://res.cloudinary.com/dq4yjeejc/video/upload/v1722331324/Magical-Moments-chosic.com__z3kesq.mp3',
+  )
 
-// Add a click event listener to the button
-viewProjectsBtn.addEventListener('click', () => {
+  // Set audio to loop
+  audio.loop = true
+
   // Play the audio
   audio.play()
+
+  // Add a click event listener to the button to play audio
+  viewProjectsBtn.addEventListener('click', () => {
+    audio.play()
+  })
 })
